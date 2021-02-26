79ef69af051dbe355f04c259fd7ab63285e2af97
Haydslife <45754776+Haydslife@users.noreply.github.com>
2021-02-26 14:15:59 +0100

    Final ver

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ var waiting = true;
 let cells = document.getElementsByClassName("cell");
 // the players sign and the ai's sign
 var playerSign = "X", aiSign = "O";
+// ways you can win
+const lines = [
+    ["a1","a2","a3"],
+    ["b1","b2","b3"],
+    ["c1","c2","c3"],
+    ["a1","b1","c1"],
+    ["a2","b2","c2"],
+    ["a3","b3","c3"],
+    ["a1","b2","c3"],
+    ["c1","b2","a3"]
+];
 
 function newGame() {
     // clear out all cells
@@ -18,16 +29,12 @@ function newGame() {
     turn = Math.floor(Math.random()*2);
 
     if (turn===1) {playerTurn()} else {aiTurn()}
-
-    console.log("1");
 }
 
 function playerTurn() {
     turn = 1;
     waiting = false;
     whoTurn.innerHTML = "Your turn!";
-
-    console.log("2");
 }
 
 function aiTurn() {
@@ -35,43 +42,80 @@ function aiTurn() {
     waiting = true;
     whoTurn.innerHTML = "Opponents turn!";
 
-    while (turn === 0) {
-        // randomly decide which cell to play
-        play = Math.floor(Math.random()*9);
-        cell = document.getElementById(cells[play].id);
+    // wait a bit before playing to mimic thinking
+    setTimeout(() => {  
+        while (turn === 0) {
+            // randomly decide which cell to play
+            play = Math.floor(Math.random()*9);
+            cell = document.getElementById(cells[play].id);
 
-        // play the cell, but only if it hasn't been played before
-        if (cell.innerHTML === "") {
-            waiting = false;
-            boardPlay("O", cell.id);
+            // play the cell, but only if it hasn't been played before
+            if (cell.innerHTML === "") {
+                waiting = false;
+                boardPlay("O", cell.id);
+            }
         }
-    }
-
-    console.log("3");
-
+    }, Math.random()*500 + 500);
 }
 
-function boardPlay(Sign, cell) {
+function boardPlay(sign, cell) {
     var cellId = document.getElementById(cell);
 
     // check if it's a valid move before doing anything (it isn't if isn't your turn or if the cell has already been played)
     if (cellId.innerHTML === "" && waiting != true) {
         // show the appropriate sign on the cell that was played
-        cellId.innerHTML = Sign;
+        cellId.innerHTML = sign;
+
+        // check if someone won
+        for (var i = 0; i < lines.length; i++) {
+            // check if a specific line was achieved
+            const line = lines[i]
+            // check if the cells with the corresponding id's have the current players sign.
+            var a = document.getElementById(line[0]).innerHTML === sign
+            var b = document.getElementById(line[1]).innerHTML === sign
+            var c = document.getElementById(line[2]).innerHTML === sign
+            // if they do, the current player wins
+            if (a && b && c) {
+                end(turn);
+                break;
+            }
+
+            // check if any cells have not been played, otherwise it is a draw
+            for (var o = 0; o < 9; o++) {
+                if (cells[o].innerHTML === "") { 
+                    break;
+                } else if (o === 8 && !a && !b && !c) {
+                    end(2);
+                }
+            }
+
+        }
 
         // Change who's turn it is
-        if (turn == 1) {
+        if (turn === 1) {
             aiTurn();
-        } else {
+        } else if (turn === 0){
             playerTurn();
         }
 
     }
-
-
-    console.log("4");
 }
 
 function end(victor) {
-    // victor is 0 if the ai wins, 1 if the player wins, and 2 if it's a stalemate.
+    // make sure that no more moves can be made
+    waiting = true;
+    turn = 2;
+
+    // show who the victor is
+    switch (victor) {
+        case 0:
+            whoTurn.innerHTML = "You lost!";
+            break;
+        case 1:
+            whoTurn.innerHTML = "You Won!";
+            break;
+        case 2:
+            whoTurn.innerHTML = "Draw!";
+            break;
+    }
 }
